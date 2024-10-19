177d2f67b2e4fe7e17086545b8c4e0577f9d342c
JosefinaSaldias <jsaldias@frsn.utn.edu.ar>
2024-10-19 19:31:22 -0300

    proyecto base

---
+++ TrabajoPractico/Proyecto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPractico
+{
+    internal abstract class Proyecto
+    {
+        private string _nombre;
+        private int _cantidadDesarrolladores;
+        private EstadoProyecto _estado;
+          
+        private DateTime _fechaInicio;
+        private DateTime _duracionEstimada;
+
+
+
+        public Proyecto(string nombre, int cantidadDesarrolladores, DateTime fechaInicio, DateTime duracionEstimada, EstadoProyecto estado )
+        {
+            this._nombre = nombre;
+            this._cantidadDesarrolladores = cantidadDesarrolladores;
+            this._fechaInicio = fechaInicio;
+            this._duracionEstimada = duracionEstimada;
+            this._estado = estado;
+        }
+
+
+
+        public abstract void VisualizarProyecto();
+        public abstract DateTime CalcularFechaEstimada();
+
+
+       
+
+ 
+
+    }
+
+
+
+}
