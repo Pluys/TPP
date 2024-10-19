2ad860c941f4a6d060668891ca851b196096fb38
Dekina <martinjeremias223@gmail.com>
2024-10-19 20:20:56 -0300

    Menu plantilla

--- TrabajoPractico/Program.cs
+++ TrabajoPractico/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main()
+    {
+        int desicion = 10;     //Navegador del menú
+        string userInput = ""; //Variable para verificar el parseo y evitar crasheos
+
+        List<Proyecto> proyectos = new List<Proyecto>();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Bienvenido.");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        while (desicion != 0 && desicion != 5) //Menu
+        {
+            desicion = 10;
+            Console.WriteLine("---Menu---\n" +
+                "0. Guardar y salir\n" +
+                "1. Agregar proyecto\n" +
+                "2. Quitar proyecto\n" +
+                "3. Modificar proyecto\n" +
+                "4. Ver proyectos\n" +
+                "5. Salir");
+
+            userInput = Console.ReadLine();
+            if (!string.IsNullOrEmpty(userInput))
+            {
+                int.TryParse(userInput, out desicion);
+            }
+
+            switch (desicion) //Opciones
+            {
+                case 0:
+                    Console.WriteLine("Guardar y salir");
+
+                    break;
+
+                case 1:
+                    Console.WriteLine("Opcion 1");
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("Opcion 2");
+
+                    break;
+
+                case 3:
+                    Console.WriteLine("Opcion 3");
+
+                    break;
+
+                case 4:
+                    Console.WriteLine("Opcion 4");
+
+                    break;
+
+                case 5:
+                    Console.WriteLine("Salir sin guardar");
+
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion invalida");
+
+                    break;
+            } //switch
+            EndMenu();
+        } //while
+
+        static void EndMenu()
+        {
+            Console.WriteLine("\nPresione cualquier tecla para continar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
